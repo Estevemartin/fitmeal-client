5e05ff85bd2eae6619157d8bd24f8a1c8be8111f
Ona Solani <onasolani@gmail.com>
2020-11-24 13:09:09 +0100

    before Deploy

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -3,8 +3,8 @@ import axios from "axios";
 class Auth {
   constructor() {
     this.auth = axios.create({
-      // baseURL: "http://localhost:4000",
-      baseURL: process.env.REACT_APP_API_URL,
+      baseURL: "http://localhost:4000",
+      // baseURL: process.env.REACT_APP_API_URL,
       withCredentials: true,
     });
   }
