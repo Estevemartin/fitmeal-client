ff11a1ba068dae8c70f9ee8132d14cbc4105ee7d
Ona Solani <onasolani@gmail.com>
2020-11-24 11:52:49 +0100

    Deploy v.3

---
+++ build/precache-manifest.e6614857bef68db702bdc16c58ce2415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037fab12560c8bd4872f89781eea25d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9932b3b4f84d0d6c352e",
+    "url": "/static/css/main.9abd4d8a.chunk.css"
+  },
+  {
+    "revision": "d6b2df4de076a3b7395f",
+    "url": "/static/js/2.1fcf32d3.chunk.js"
+  },
+  {
+    "revision": "61e6efc66162968c071666519a5e34c5",
+    "url": "/static/js/2.1fcf32d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9932b3b4f84d0d6c352e",
+    "url": "/static/js/main.1eaa2471.chunk.js"
+  },
+  {
+    "revision": "5179ff928fc7dafdbfe9",
+    "url": "/static/js/runtime-main.81fc7dff.js"
+  }
+]);
